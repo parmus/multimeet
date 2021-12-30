ada99526d0588fc47716749d0121b141ef732aa5
Martin Parm <martin.parm@peltarion.com>
2021-12-30 16:58:31 +0100

    Center login buton on page

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,18 +1,30 @@
-import { Button } from '@mui/material';
+import { Button, Grid } from '@mui/material';
 import { useNavigate } from "react-router-dom";
 import { useEffect } from 'react';
 
 export const LoginPage = ({ auth }) => {
-    const navigate = useNavigate();
-    const loggedIn = auth.isSignedIn.get();
+  const navigate = useNavigate();
+  const loggedIn = auth.isSignedIn.get();
 
-    useEffect(() => {
-        if (loggedIn) navigate('/')    
-    }, [loggedIn, navigate])
+  useEffect(() => {
+    if (loggedIn) navigate('/')
+  }, [loggedIn, navigate])
 
-    if (loggedIn) return null
+  if (loggedIn) return null
 
-    return (
-        <Button onClick={() => auth.signIn()} color="inherit">Login</Button>        
-    )
+  return (
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
+    >
+      <h3>MultiMeet</h3>
+      <Button onClick={() => auth.signIn()} color="inherit">
+        Login
+      </Button>
+    </Grid>
+  )
 }
